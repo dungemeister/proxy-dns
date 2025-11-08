3f97cc4f256f76dcb9fc59e81ee667e622e3f269
dungemeister <yuriy.vazha@gmail.com>
2025-11-08 14:13:55 +0400

    Updated tests

--- tests/test_proxy-dns.py
+++ tests/test_proxy-dns.py
@@ -1,24 +1,78 @@
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+TESTS_DIR = SCRIPT_DIR
+
+def start_dig(proxy_ip: str, proxy_port: str, domain) -> str:
+    process = Popen(f"dig @{proxy_ip} -p {proxy_port} {domain}", stdout=PIPE, stderr=STDOUT, shell=True)
+    stdout, _ = process.communicate()
+    return stdout.decode()
+
+def parse_output(output:str, requested: str) -> bool:
+    answer: bool = False
+    res: bool = False
+    for line in output.split("\n"):
+        if "Got answer:" in line:
+            answer = True
+        if "->>HEADER<<-" in line:
+            for token in line.split(", "):
+                if "status" in token:
+                    assert requested == token.split(": ")[1]
+    assert answer == True
+
+def start_proxy_dns(config_path: str) -> str:
+    if config_path is None or config_path == "":
+        assert config_path != ""
+    process = Popen(f"{SCRIPT_DIR}/../proxy-dns {config_path}", stdout=PIPE, stderr=STDOUT, shell=True, close_fds=True)
 
 def test_blacklist_refused():
-    res = True
-    assert res == True
+    blacklist = ["google.com"]
+    for domain in blacklist:
+        start_proxy_dns(f"{TESTS_DIR}/test1.config")
+        output = start_dig("127.0.0.1", "6969", domain)
+        parse_output(output, "REFUSED")
+    
 
 def test_blacklist_not_found():
-    res = True
-    assert res == True
+    blacklist = ["ya.ru"]
+    for domain in blacklist:
+        start_proxy_dns(f"{TESTS_DIR}/test2.config")
+        output = start_dig("127.0.0.1", "6969", domain)
+        parse_output(output, "NXDOMAIN")
 
 def test_blacklist_readressing():
-    res = True
-    assert res == True
+    blacklist = ["google.com"]
+    for domain in blacklist:
+        start_proxy_dns(f"{TESTS_DIR}/test3.config")
+        output = start_dig("127.0.0.1", "6969", domain)
+        parse_output(output, "NXDOMAIN")
 
 def test_existed_domain():
-    res = True
-    assert res == True
+    blacklist = ["google.com"]
+    for domain in blacklist:
+        start_proxy_dns(f"{TESTS_DIR}/test4.config")
+        output = start_dig("127.0.0.1", "6969", domain)
+        parse_output(output, "NXDOMAIN")
 
 def test_not_existed_domain():
-    res = True
-    assert res == True
+    blacklist = ["google.com"]
+    for domain in blacklist:
+        start_proxy_dns(f"{TESTS_DIR}/test5.config")
+        output = start_dig("127.0.0.1", "6969", domain)
+        parse_output(output, "NXDOMAIN")
 
 def test_not_existed_upstream():
-    res = True
-    assert res == True
+    blacklist = ["google.com"]
+    for domain in blacklist:
+        start_proxy_dns(f"{TESTS_DIR}/test6.config")
+        output = start_dig("127.0.0.1", "6969", domain)
+        parse_output(output, "NXDOMAIN")
+
+def test_correct_queries_with_huge_clacklist():
+    blacklist = ["google.com"]
+    for domain in blacklist:
+        start_proxy_dns(f"{TESTS_DIR}/test7.config")
+        output = start_dig("127.0.0.1", "6969", domain)
+        parse_output(output, "NXDOMAIN")
