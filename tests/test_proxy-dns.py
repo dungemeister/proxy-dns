f06be4ff563a887927c4568dc140fd66a3fd0321
dungemeister <yuriy.vazha@gmail.com>
2025-11-09 17:08:15 +0400

    Updated tests

--- tests/test_proxy-dns.py
+++ tests/test_proxy-dns.py
@@ -1,9 +1,13 @@
-import subprocess
+import subprocess, psutil
 from subprocess import Popen, PIPE, STDOUT
 import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 TESTS_DIR = SCRIPT_DIR
+
+PROXY_DNS_NAME = "proxy-dns"
+PROXY_DNS_DIR = f"{SCRIPT_DIR}/../build"
+PROXY_DNS_APP_PATH = f"{PROXY_DNS_DIR}/{PROXY_DNS_NAME}"
 
 def start_dig(proxy_ip: str, proxy_port: str, domain) -> str:
     process = Popen(f"dig @{proxy_ip} -p {proxy_port} {domain}", stdout=PIPE, stderr=STDOUT, shell=True)
@@ -29,64 +33,85 @@ def parse_output(output:str, requested_str: str, requested_ip: str = "") -> bool
     
     assert got_answer == True
 
-def start_proxy_dns(config_path: str) -> str:
+def start_proxy_dns(config_path: str) -> Popen:
     if config_path is None or config_path == "":
         assert config_path != ""
-    process = Popen(f"{SCRIPT_DIR}/../proxy-dns {config_path}", stdout=PIPE, stderr=STDOUT, shell=True, close_fds=True)
+    return Popen(f"{PROXY_DNS_APP_PATH} {config_path}", stdout=PIPE, stderr=STDOUT, shell=True)
+
+def kill_process_tree(pid):
+    try:
+        parent = psutil.Process(pid)
+        children = parent.children(recursive=True)
+        for child in children:
+            child.kill()
+        parent.kill()
+    except psutil.NoSuchProcess:
+        pass
 
 def test_blacklist_refused():
     blacklist = ["google.com"]
+    process = start_proxy_dns(f"{TESTS_DIR}/{test_blacklist_refused.__name__}.config")
     for domain in blacklist:
-        start_proxy_dns(f"{TESTS_DIR}/{test_blacklist_refused.__name__}.config")
         output = start_dig("127.0.0.1", "6969", domain)
         parse_output(output, "REFUSED")
-    
+    kill_process_tree(process.pid)
 
 def test_blacklist_not_found():
     blacklist = ["ya.ru"]
+    process = start_proxy_dns(f"{TESTS_DIR}/{test_blacklist_not_found.__name__}.config")
     for domain in blacklist:
-        start_proxy_dns(f"{TESTS_DIR}/{test_blacklist_not_found.__name__}.config")
         output = start_dig("127.0.0.1", "6969", domain)
         parse_output(output, "NXDOMAIN")
+    kill_process_tree(process.pid)
 
 def test_blacklist_readressing():
     blacklist = [("yandex.ru", "10.10.10.10")]
+    process = start_proxy_dns(f"{TESTS_DIR}/{test_blacklist_readressing.__name__}.config")
     for domain, r_ip in blacklist:
-        start_proxy_dns(f"{TESTS_DIR}/{test_blacklist_readressing.__name__}.config")
         output = start_dig("127.0.0.1", "6969", domain)
         parse_output(output, "NOERROR", r_ip)
+    kill_process_tree(process.pid)
 
 def test_existed_domain():
     blacklist = ["github.com"]
+    process = start_proxy_dns(f"{TESTS_DIR}/{test_existed_domain.__name__}.config")
     for domain in blacklist:
-        start_proxy_dns(f"{TESTS_DIR}/{test_existed_domain.__name__}.config")
         output = start_dig("127.0.0.1", "6969", domain)
         parse_output(output, "NOERROR")
+    kill_process_tree(process.pid)
 
 def test_not_existed_domain():
     blacklist = ["googlerrob.com.123.123"]
+    process = start_proxy_dns(f"{TESTS_DIR}/{test_not_existed_domain.__name__}.config")
     for domain in blacklist:
-        start_proxy_dns(f"{TESTS_DIR}/{test_not_existed_domain.__name__}.config")
         output = start_dig("127.0.0.1", "6969", domain)
         parse_output(output, "NXDOMAIN")
+    kill_process_tree(process.pid)
 
 def test_not_existed_upstream():
     blacklist = ["github.com"]
+    process = start_proxy_dns(f"{TESTS_DIR}/{test_not_existed_upstream.__name__}.config")
     for domain in blacklist:
-        start_proxy_dns(f"{TESTS_DIR}/{test_not_existed_upstream.__name__}.config")
         output = start_dig("127.0.0.1", "6969", domain)
         parse_output(output, "SERVFAIL")
+    kill_process_tree(process.pid)
 
 def test_existed_upstream():
     blacklist = ["github.com"]
+    process = start_proxy_dns(f"{TESTS_DIR}/{test_existed_upstream.__name__}.config")
     for domain in blacklist:
-        start_proxy_dns(f"{TESTS_DIR}/{test_existed_upstream.__name__}.config")
         output = start_dig("127.0.0.1", "6969", domain)
         parse_output(output, "NOERROR")
+    kill_process_tree(process.pid)
 
 def test_correct_queries_with_huge_blacklist():
     blacklist = ["github.com"]
+    process = start_proxy_dns(f"{TESTS_DIR}/{test_correct_queries_with_huge_blacklist.__name__}.config")
     for domain in blacklist:
-        start_proxy_dns(f"{TESTS_DIR}/{test_correct_queries_with_huge_blacklist.__name__}.config")
         output = start_dig("127.0.0.1", "6969", domain)
         parse_output(output, "NOERROR")
+    kill_process_tree(process.pid)
+
+
+if __name__ == "__main__":
+    test_not_existed_upstream()
